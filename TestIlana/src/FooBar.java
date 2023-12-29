2574d3f48a236d2d535eef4316e88f425ea3e34d
lusilva <lusilva@heinsohn.com.co>
2023-12-29 18:18:25 -0500

    Actualizacion de comentarios

--- TestIlana/src/FooBar.java
+++ TestIlana/src/FooBar.java
@@ -1,31 +1,50 @@
 import java.util.Scanner;
 
+/**
+ * Clase creada para realizar el ejercicio del Foo Bar
+ */
 public class FooBar {
 
     private static int num;
 
     public static void main(String[] argumentos) {
 
+        // Lineas para capturar por consola el número
         System.out.println("Digite el número: ");
         Scanner scanner = new Scanner(System.in);
         String entrada = scanner.next();
 
+        /**
+         * Método que devuelve la validación del dato digitado
+         */
         if (!esNumero(entrada)) {
             System.out.println("no es un número");
             return;
         }
 
+        // Valida si el número es divisible por 3 y por 5
         if (num % 3 == 0 && num % 5 == 0) {
+            // Linea para imprimir la salida, si es verdadera la validación
             System.out.print("FooBar");
+            // Valida si el número es divisible por 5
         } else if (num % 5 == 0) {
+            // Linea para imprimir la salida, si es verdadera la validación
             System.out.print("Bar");
+            //Valida si el número es divisible por 3
         } else if (num % 3 == 0) {
+            // Linea para imprimir la salida, si es verdadera la validación
             System.out.print("Foo");
+            //Imprime el número si no cumple con ninguna validación
         } else {
             System.out.print(num);
         }
     }
 
+    /**
+     * Método para validar que el dato sea un número.
+     * @param str
+     * @return
+     */
     public static boolean esNumero(String str) {
         try { //Intentar convertir el String a un número
             num=Integer.parseInt(str);
--- TestIlana/src/NumeroFeliz.java
+++ TestIlana/src/NumeroFeliz.java
@@ -8,44 +8,67 @@ public class NumeroFeliz {
 
     public static void main(String[] argumentos) {
 
+        // Lineas para capturar por consola el número
         System.out.println("Digite el número: ");
         Scanner scanner = new Scanner(System.in);
         num = scanner.next();
 
+        /**
+         * Método que devuelve la validación del dato digitado
+         */
         if (!esNumero(num)) {
             System.out.println("no es un número");
             return;
         }
+        //Línea para asignar una lista para los resultados de las sumas de los números
         List<Integer> listaResultado = new ArrayList<>();
+        //Separa cada digito del número digitado
         String[] numeroLista = num.split("");
 
-        Integer resultado=0;
-        boolean validacion=true;
-        while (validacion){
+        // variable para asignar el resultado
+        Integer resultado = 0;
+        //variable creada para salir del while
+        boolean validacion = true;
+        while (validacion) {
+            //for creado para sumar los números y luego el resultado de la suma de esos números
             for (String numero : numeroLista) {
+                //asigna la sumatoria a resultado
                 resultado += (Integer.parseInt(numero) * Integer.parseInt(numero));
             }
+            //se asigna resultado a la lista de resultados
             listaResultado.add(resultado);
+            //se asigna resultado a la variable num para luego compararlo
             num = resultado.toString();
+            //Se valida que si el resultado es igual a 1 se salga del ciclo porque ya es un número feliz
             if (resultado == 1) {
-                validacion=false;
+                validacion = false;
                 break;
             }
-            int i=0;
-            for(Integer item : listaResultado){
-                if(item.intValue()==resultado.intValue()){
+            //se almacena los resultados
+            int i = 0;
+            for (Integer item : listaResultado) {
+                if (item.intValue() == resultado.intValue()) {
                     i++;
                 }
             }
-            if(i>=2){
+            // Se valida si se repiten los resultados, se salga del ciclo
+            if (i >= 2) {
                 break;
             }
-            resultado=0;
+            //limpiar la variable resultado para inicializar el ciclo
+            resultado = 0;
+            //se asigna el valor que tiene la variable num
             numeroLista = num.split("");
         }
+        //imprime la validación del ejercicio
         System.out.println("el resultado es: " + !validacion);
     }
 
+    /**
+     * Método para validar que el dato sea un número.
+     * @param str
+     * @return
+     */
     public static boolean esNumero(String str) {
         try { //Intentar convertir el String a un número
             Integer.parseInt(str);
@@ -55,6 +78,4 @@ public class NumeroFeliz {
             return false;
         }
     }
-
-
 }
